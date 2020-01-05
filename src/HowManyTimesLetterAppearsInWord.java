ef14d27aa0ed0566e1538477b9deff7bea402a65
ka5ta <kasta.urbanska@gmail.com>
2020-01-05 19:03:32 +0100

    First commit number one

---
+++ src/HowManyTimesLetterAppearsInWord.java
@@ -0,0 +1,33 @@
+public class HowManyTimesLetterAppearsInWord {
+
+    public static void main(String[] args) {
+
+    String str = "KATARZYNA";
+    char[] c = str.toCharArray();
+    int count = 0;
+    char positionInText='0';
+
+
+    StringBuilder text = new StringBuilder();
+
+        for (int i = 0; i <c.length ; i++) {
+            StringBuilder sequence = new StringBuilder();
+            for (int j = 0; j <c.length ; j++) {
+                boolean isOnCheckedPosition=false;
+                if(c[i]==c[j]){
+                    count +=1;
+                    positionInText ='1';
+                    isOnCheckedPosition=true;
+                }
+                if(!isOnCheckedPosition){
+                    positionInText='*';
+                }
+                sequence.append(positionInText);  // 1
+            }
+        text.append(c[i]).append(" -> ").append(sequence)
+                .append(" and appeared: ").append(count).append(" times. ").append("\n");
+            count = 0;
+        }
+        System.out.println(text);
+    }
+}
