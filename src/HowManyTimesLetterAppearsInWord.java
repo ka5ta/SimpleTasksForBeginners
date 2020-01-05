54b2c02e7ac86108895099ceb32baaa107d9a145
ka5ta <kasta.urbanska@gmail.com>
2020-01-05 21:54:08 +0100

    rebuilt code, simplified statements and added doc comment

--- src/HowManyTimesLetterAppearsInWord.java
+++ src/HowManyTimesLetterAppearsInWord.java
@@ -1,45 +1,46 @@
 public class HowManyTimesLetterAppearsInWord {
 
+    /**
+     * Display only once the letters in a word and show where in the text they appeared
+     * and how many times.
+     * @param args
+     */
+
     public static void main(String[] args) {
 
-    String str = "KATARZYNA";
-    char[] c = str.toCharArray();
-    int count = 0;
-    char positionInText='0';
+        String str = "KATARZYNA";
+        char[] c = str.toCharArray();
+        int count = 0;
+        
+        StringBuilder text = new StringBuilder();
 
-
-    StringBuilder text = new StringBuilder();
-
-        for (int i = 0; i <c.length ; i++) {
-            if(!isFirstOccurrence(c, i)){
+        for (int i = 0; i < c.length; i++) {
+            //skips loop iteration if current letter already appeared
+            if (!firstOccurrence(c, i)) {
                 continue;
             }
             StringBuilder sequence = new StringBuilder();
-            for (int j = 0; j <c.length ; j++) {
-                boolean isOnCheckedPosition=false;
-                if(c[i]==c[j]){
-                    count +=1;
-                    positionInText ='1';
-                    isOnCheckedPosition=true;
+            for (int j = 0; j < c.length; j++) {
+                boolean isOnCheckedPosition = false;
+                if (c[i] == c[j]) {
+                    count += 1;
+                    isOnCheckedPosition = true;
                 }
-                if(!isOnCheckedPosition){
-                    positionInText='*';
-                }
-                sequence.append(positionInText);  // 1
+                sequence.append(isOnCheckedPosition?'1':'*');  // 10
             }
-        text.append(c[i]).append(" -> ").append(sequence)
-                .append(" and appeared: ").append(count).append(" times. ").append("\n");
+            text.append(c[i]).append(" -> ").append(sequence)
+                    .append(" and appeared: ").append(count).append(" times. ").append("\n");
             count = 0;
         }
         System.out.println(text);
     }
 
-    private static boolean isFirstOccurrence(char[] words, int index) {
-        for (int i = 0; i <index ; i++) {
-            if(words[i]==words[index]){
-                return false;
+    private static boolean firstOccurrence(char[] words, int index) {
+        for (int i = 0; i < index; i++) {
+            if (words[i] == words[index]) {
+                return false; //when checked letter appeared already, it is not the first occurrence
             }
         }
-        return true;
+        return true; //the checked letter didn't appear before index
     }
 }
