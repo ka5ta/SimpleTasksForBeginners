710abee110da44808205700b57e13bd074c0d5fe
ka5ta <kasta.urbanska@gmail.com>
2020-01-24 22:10:49 +0100

    adding method 'getRandomKey' which returns pairs of letters where the probability increases with number of occurrence.

--- src/_00_NameGenerator/Main.java
+++ src/_00_NameGenerator/Main.java
@@ -1,28 +1,16 @@
 package _00_NameGenerator;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    static Random rd = new Random();
 
-        URL url = new URL("https://www.gutenberg.org/files/47/47-h/47-h.htm");
+    public static Map<String, Integer> letterPairOccurrence(String text) {
 
-        //--I got text from URL without HTML components--//
-        //String text = Html2Text.parse(new InputStreamReader(url.openStream()));
-
-        //--Remove special characters, numbers and punctuation--//
-        //text=text.replaceAll("[^a-zA-Z\\s+]", "");
-
-        String text = "lambadaba balamba";
-
-        //labamba balamba
+        //Create Map with key and the value of it occurrence
         Map<String,Integer> statistics = new HashMap<>();
 
         for (int i = 0; i <text.length()-1 ; i++) {
@@ -44,10 +32,63 @@ public class Main {
                 statistics.put(checkedLetters, 1);
             }*/
         }
+        return statistics;
+    }
 
-        statistics.entrySet().forEach(entry ->
-                System.out.printf("Letters: %s, Count: %d\n",entry.getKey(),entry.getValue()));
+    public static String getRandomKey(Map<String,Integer> statistics)  {
+
+
+        /*List<String> keys = new ArrayList<String>(statistics.keySet());
+        int randomLettersIndex = rd.nextInt(keys.size());
+        String randomLetters = keys.get(randomLettersIndex);*/
+
+        //This function will return sum of values form hashmap
+        List<Integer> valuesList = new ArrayList<>(statistics.values());
+        int sumOfValues = valuesList.stream()
+                .mapToInt(value->value)
+                .sum();
+
+        //Generate random index form the sum of all keys
+        int randomIndex = rd.nextInt(sumOfValues) + 1;
+
+        //Get the key that corresponds to the randomIndex where the probability increase with number of occurrences.
+        int sumIndex = 0;
+        for (Map.Entry<String,Integer> key : statistics.entrySet()) {
+            sumIndex += key.getValue();
+            if(sumIndex>randomIndex){
+                return key.getKey();
+            }
+        }
+        throw new RuntimeException("Something wrong with key value - might be null");
+    }
+
+    public static void getRandomNames(Map<String, Integer> statistics, int howManyNamesReturn){
+    //todo probably last method that will generate randomly length for names
+        int nameLength = rd.nextInt(8)+1;
+        getRandomKey(statistics);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        URL url = new URL("https://www.gutenberg.org/files/47/47-h/47-h.htm");
+
+        //--I got text from URL without HTML components--//
+        String text = Html2Text.parse(new InputStreamReader(url.openStream()));
+
+        //--Remove special characters, numbers and punctuation--//
+        text=text.replaceAll("[^a-zA-Z\\s+]", "");
+
+        Map<String, Integer> statistics = letterPairOccurrence(text);
+        getRandomNames(statistics,5);
+
+        /*statistics.entrySet().forEach(entry ->
+                System.out.printf("Letters: %s, Count: %d\n",entry.getKey(),entry.getValue()));*/
+
+
+
 
     }
+
+
 
 }
